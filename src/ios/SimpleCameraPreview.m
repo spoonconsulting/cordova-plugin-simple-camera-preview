da1cd7b8d53223d4ba93b58f2546f7af55b005a3
Mevin Dhunnooa <mdhunnooa@gmail.com>
2018-09-20 10:50:09 +0400

    insert camera view at the back

--- src/ios/SimpleCameraPreview.m
+++ src/ios/SimpleCameraPreview.m
@@ -27,10 +27,9 @@
     // render controller setup
     self.cameraRenderController = [[CameraRenderController alloc] init];
     self.cameraRenderController.sessionManager = self.sessionManager;
-    self.cameraRenderController.view.frame = CGRectMake(0, [UIApplication sharedApplication].statusBarFrame.size.height, self.viewController.view.frame.size.width, self.viewController.view.frame.size.height);
+    self.cameraRenderController.view.frame = CGRectMake(0, 0, self.viewController.view.frame.size.width, self.viewController.view.frame.size.height);
     [self.viewController addChildViewController:self.cameraRenderController];
-    [self.webView.superview addSubview:self.cameraRenderController.view];
-    [self.webView.superview bringSubviewToFront:self.webView];
+    [self.webView.superview insertSubview:self.cameraRenderController.view atIndex:0];
     
     // Setup session
     self.sessionManager.delegate = self.cameraRenderController;
