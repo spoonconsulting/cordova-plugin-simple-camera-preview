9765c218651a3309f507bc1b9dcca95cac2309cb
J4tinP <jatin.purmessur@spoonconsulting.com>
2025-02-03 11:59:10 +0400

    fix missing parameter

--- src/android/SimpleCameraPreview.java
+++ src/android/SimpleCameraPreview.java
@@ -89,7 +89,7 @@ public class SimpleCameraPreview extends CordovaPlugin {
                     return deviceHasFlash(callbackContext);
 
                 case "deviceHasUltraWideCamera":
-                    return deviceHasUltraWideCamera(callbackContext);
+                    return deviceHasUltraWideCamera(args.getString(0),callbackContext);
 
                 case "switchCameraTo":
                     return switchCameraTo((JSONObject) args.get(0), callbackContext);
