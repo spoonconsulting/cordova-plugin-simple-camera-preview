9caa0c1fc25350ae9f63011174a7eea0376c226b
J4tinP <jatin.purmessur@spoonconsulting.com>
2025-07-21 15:42:49 +0400

    Adds dual video capture functions to DualCameraPreview

--- www/DualCameraPreview.js
+++ www/DualCameraPreview.js
@@ -20,4 +20,28 @@ DualCameraPreview.disableDualMode = function (onSuccess, onError) {
   exec(onSuccess, onError, PLUGIN_NAME, "disableDualMode", []);
 };
 
+DualCameraPreview.startVideoCaptureDual = function (options, onSuccess, onError) {
+  if (!DualCameraPreview.videoCallback) {
+    console.error("Call initVideoCallback first");
+    onError("Call initVideoCallback first");
+    return;
+  }
+
+  if (!DualCameraPreview.videoInitialized) {
+    console.error("videoCallback not initialized");
+    onError("videoCallback not initialized");
+    return;
+  }
+  
+  options = options || {};
+  options.recordWithAudio = options.recordWithAudio != null ? options.recordWithAudio : true;
+  options.videoDurationMs = options.videoDurationMs != null ? options.videoDurationMs : 3000;
+  exec(onSuccess, onError, PLUGIN_NAME, "startVideoCaptureDual", [options]);
+};
+
+DualCameraPreview.stopVideoCaptureDual = function (onSuccess, onError) {
+  exec(onSuccess, onError, PLUGIN_NAME, "stopVideoCaptureDual");
+};
+
+
 module.exports = DualCameraPreview;
